7567403dacaa5d1664d07b193d616b9055e06992
Han <601107w@gmail.com>
2019-02-22 18:26:56 +0900

    main.js分離

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-
-var app = new Vue({
-	el:'#app',
-	data: {
+new Vue({ 
+  	el: '#app',
+  	data: {
 		items: ['Foo','Bar','Fizz','Buzz'],
-        checkbox:true,
-        radioGroup: 1,
-        switch1: true,
-        align:'align-center'
-	}
+    radioGroup: 1
+	} 
 })
