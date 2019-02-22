e4047f2daff08016289e4a29fcb6d44785d9f55a
Han <601107w@gmail.com>
2019-02-22 18:20:49 +0900

    vue-cliを使ってない初めてのvuetify

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var app = new Vue({
+	el:'#app',
+	data: {
+		items: ['Foo','Bar','Fizz','Buzz'],
+        checkbox:true,
+        radioGroup: 1,
+        switch1: true,
+        align:'align-center'
+	}
+})
