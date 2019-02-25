bc323d9d22ed007b34af91e6656dec9b6c975a94
Han <601107w@gmail.com>
2019-02-25 14:03:27 +0900

    rulesとradio-groupの勉強

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 new Vue({ 
   	el: '#app',
-  	data: {
-		items: ['Foo','Bar','Fizz','Buzz'],
-    radioGroup: 1
-	} 
+  	data:() =>({
+			items: ['Foo','Bar','Fizz','Buzz'],
+    	radioGroup: 1,
+    	rules:{
+    		Uppercase:value => {
+    			const pattern = /^([A-Z])*$/
+          return pattern.test(value) || 'Just Input Uppercase'
+    		}
+    	}
+	})
 })
